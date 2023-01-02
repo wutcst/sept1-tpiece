09a8f11d2998262f7a9411c814b441ca5ca03130
tpiece <105487531+tpiece@users.noreply.github.com>
2023-01-02 22:09:05 +0800

    Create CommandWordsTest.java

---
+++ src/cn/edu/whut/sept/zuul/CommandWordsTest.java
@@ -0,0 +1,14 @@
+package cn.edu.whut.sept.zuul;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CommandWordsTest {
+
+    @Test
+    public void isCommand() {
+        CommandWords commandwords=new CommandWords();
+        assertEquals(true,commandwords.isCommand("back"));
+    }
+}

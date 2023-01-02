06da6f642beb6ff38500d9bf3a23cae0a0078d62
tpiece <105487531+tpiece@users.noreply.github.com>
2023-01-02 22:08:20 +0800

    Create CommandTest.java

---
+++ src/cn/edu/whut/sept/zuul/CommandTest.java
@@ -0,0 +1,32 @@
+package cn.edu.whut.sept.zuul;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CommandTest {
+
+    @Test
+    public void getCommandWord() {
+        Command command=new Command("firstword","secondword");
+        assertEquals("firstword",command.getCommandWord());
+    }
+
+    @Test
+    public void getSecondWord() {
+        Command command=new Command("firstword","secondword");
+        assertEquals("secondword",command.getSecondWord());
+    }
+
+    @Test
+    public void isUnknown() {
+        Command command=new Command("firstword","secondword");
+        assertEquals(false,command.isUnknown());
+    }
+
+    @Test
+    public void hasSecondWord() {
+        Command command=new Command("firstword","secondword");
+        assertEquals(true,command.hasSecondWord());
+    }
+}

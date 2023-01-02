126ce1210dcc14483063f9631373f79a4f61e77e
tpiece <105487531+tpiece@users.noreply.github.com>
2023-01-02 22:05:02 +0800

    Update CommandWords.java

--- src/cn/edu/whut/sept/zuul/CommandWords.java
+++ src/cn/edu/whut/sept/zuul/CommandWords.java
@@ -2,15 +2,26 @@ package cn.edu.whut.sept.zuul;
 
 public class CommandWords
 {
+    /**
+    *返回提示信息
+     */
     private static final String[] validCommands = {
-            "go", "quit", "help"
+            "go", "quit", "help","back","look"
     };
 
+    /**
+     * 构造函数
+     */
     public CommandWords()
     {
         // nothing to do at the moment...
     }
 
+    /**
+     * 识别传入的字符串是否为命令.
+     * @param aString 传入一个字符串.
+     * @return 返回一个布尔值.
+     */
     public boolean isCommand(String aString)
     {
         for(int i = 0; i < validCommands.length; i++) {
@@ -20,6 +31,10 @@ public class CommandWords
         return false;
     }
 
+    /**
+     * 方法：显示全部命令.
+     * 用于显示全部命令.
+     */
     public void showAll()
     {
         for(String command: validCommands) {
